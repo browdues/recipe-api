9e0c5921ada27c16d6b349cce9f62c160fcf5067
Nicholas Browdues <browdues@gmail.com>
2019-11-18 23:23:21 -0700

    Added tests and functionality for creating an auth token

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -7,6 +7,7 @@ from rest_framework import status
 
 
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
 
 
 def create_user(**params):
@@ -54,3 +55,38 @@ class PublicUserApiTests(TestCase):
         ).exists()
 
         self.assertFalse(user_exists)
+
+    def create_token_for_user(self):
+        """Test that a token is created for the user"""
+        payload = {'email': 'test@example.com', 'password': 'test123'}
+        create_user(**payload)
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_invalid_credentials(self):
+        """Test that token is NOT created when invalid credentials are given"""
+        create_user(**{'email': 'test@example.com', 'password': 'test123'})
+        payload = {'email': 'test@example.com', 'password': 'wrong'}
+
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_with_no_user(self):
+        """Test that token is not created if user does not exist"""
+        create_user(**{'email': 'test@example.com', 'password': 'test123'})
+        payload = {'email': 'nottest@example.com', 'password': 'test123'}
+
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_missing_field(self):
+        res = self.client.post(
+            TOKEN_URL, {'email': 'test@example.com', 'password': ''})
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
