b0399d5c81ae5181db77300f6b62bacba1a8b81a
Nicholas Browdues <browdues@gmail.com>
2019-11-21 14:13:45 -0700

    Fixed a lint issue

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -51,7 +51,8 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(res.data, serializer.data)
 
     def test_ingredients_limited_to_user(self):
-        """Test that only ingredients for the authenticated user are returned"""
+        """Test that only ingredients for the authenticated user are
+        returned"""
         user2 = get_user_model().objects.create_user(
             'otherUser@example.com'
             'test123'
